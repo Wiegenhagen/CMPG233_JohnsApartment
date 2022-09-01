4654e8c4c69931a1f7a41a009741ea032eb240c6
arina <wiegenhagena@gmail.com>
2022-09-01 15:46:51 +0200

    Added the connection string for the database on Form1

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 
 namespace Group_26_Johns_RealEstate_Management_System
@@ -16,6 +17,14 @@ namespace Group_26_Johns_RealEstate_Management_System
         public Form1()
         {
             InitializeComponent();
+            SqlConnection conn = Connect();
+        }
+
+        public SqlConnection Connect()
+        { 
+            //Verander die Data Source = na julle PC se naam, kyk wat julle connection string is. Data Source=""DESKTOP-LNM37V2 <--- verander die"
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-LNM37V2;Initial Catalog=JOHNS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;User ID=johnsSystem;Password=password;"));
+            return conn;
         }
     }
 }
