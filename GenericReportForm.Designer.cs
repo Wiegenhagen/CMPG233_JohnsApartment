9a07a630ef908c463097538df3c6291dc747b7f3
Arina Wiegenhagen <wiegenhagena@gmail.com>
2022-09-13 12:48:23 +0200

    generic Report form colour change

--- GenericReportForm.Designer.cs
+++ GenericReportForm.Designer.cs
@@ -40,29 +40,31 @@ namespace Group_26_Johns_RealEstate_Management_System
             // 
             // btnSave
             // 
+            this.btnSave.BackColor = System.Drawing.Color.Silver;
             this.btnSave.Enabled = false;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.ForeColor = System.Drawing.Color.Blue;
-            this.btnSave.Location = new System.Drawing.Point(1157, 481);
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSave.Location = new System.Drawing.Point(912, 449);
             this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(245, 79);
+            this.btnSave.Size = new System.Drawing.Size(229, 76);
             this.btnSave.TabIndex = 18;
             this.btnSave.Text = "Save As";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnBack
             // 
+            this.btnBack.BackColor = System.Drawing.Color.Silver;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBack.ForeColor = System.Drawing.Color.Blue;
-            this.btnBack.Location = new System.Drawing.Point(15, 481);
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBack.Location = new System.Drawing.Point(246, 450);
             this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(245, 79);
-            this.btnBack.TabIndex = 17;
+            this.btnBack.Size = new System.Drawing.Size(223, 74);
+            this.btnBack.TabIndex = 12;
             this.btnBack.Text = "Back to Home Page";
-            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // lblBankName
@@ -76,15 +78,17 @@ namespace Group_26_Johns_RealEstate_Management_System
             // 
             // btnShow
             // 
+            this.btnShow.BackColor = System.Drawing.Color.Silver;
+            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShow.ForeColor = System.Drawing.Color.Blue;
-            this.btnShow.Location = new System.Drawing.Point(588, 481);
+            this.btnShow.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnShow.Location = new System.Drawing.Point(577, 449);
             this.btnShow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(245, 79);
+            this.btnShow.Size = new System.Drawing.Size(229, 76);
             this.btnShow.TabIndex = 15;
             this.btnShow.Text = "Preview of Report";
-            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.UseVisualStyleBackColor = false;
             this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
             // 
             // lstbxFullReport
@@ -113,6 +117,7 @@ namespace Group_26_Johns_RealEstate_Management_System
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(1455, 606);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnSave);
@@ -120,7 +125,7 @@ namespace Group_26_Johns_RealEstate_Management_System
             this.Controls.Add(this.lblBankName);
             this.Controls.Add(this.btnShow);
             this.Controls.Add(this.lstbxFullReport);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "GenericReportForm";
             this.Text = "GenericReportForm";
             this.ResumeLayout(false);
